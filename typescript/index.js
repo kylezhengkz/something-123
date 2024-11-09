d231e496634e3ac06c866f57bcd071f47325fd41
Kyle <kylezhengkz@gmail.com>
2024-11-09 16:37:42 -0500

    new learn

---
+++ typescript/index.js
@@ -0,0 +1,23 @@
+var firstName = "Dylan"; // explicit typing (writing out type)
+var otherGuy = "Bobby"; // inplicit typing - will infer type based on assigned value
+// otherGuy = 9; // does not allow this
+console.log(firstName);
+console.log(typeof firstName);
+function greet(val) {
+    console.log("Hello, ".concat(val)); // note the backticks - called template literals (string interpolation)
+    val = "Timmy"; // allowed
+    console.log("Hello again but this time you are ".concat(val));
+    // can be dangerous, since it ignores type errors
+    // val.push("Bob");
+}
+greet(50);
+// unknown is a safer alternative to any
+function greet1(val) {
+    console.log("Hello, ".concat(val));
+    // cannot assign to unknown type, so below would be an error
+    // let s1: number = val;
+    // does not ignore type errors, so below would be an error
+    // val.push("Bob");
+}
+greet1(50);
+greet1("Disney");
---
+++ typescript/index.ts
@@ -0,0 +1,32 @@
+let firstName: string = "Dylan"; // explicit typing (writing out type)
+let otherGuy = "Bobby"; // inplicit typing - will infer type based on assigned value
+
+// otherGuy = 9; // does not allow this
+
+console.log(firstName);
+
+console.log(typeof firstName)
+
+function greet(val: any) {
+    console.log(`Hello, ${val}`); // note the backticks - called template literals (string interpolation)
+
+    val = "Timmy"; // allowed
+    console.log(`Hello again but this time you are ${val}`);
+    
+    // can be dangerous, since it ignores type errors
+    // val.push("Bob");
+}
+greet(50);
+
+// unknown is a safer alternative to any
+function greet1(val: unknown) {
+    console.log(`Hello, ${val}`);
+    
+    // cannot assign to unknown type, so below would be an error
+    // let s1: number = val;
+    
+    // does not ignore type errors, so below would be an error
+    // val.push("Bob");
+}
+greet1(50);
+greet1("Disney");
