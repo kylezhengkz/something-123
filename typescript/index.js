510afc75f989f16c03547bfd6e21248a922c5620
Kyle <kylezhengkz@gmail.com>
2024-11-09 16:41:36 -0500

    reorg

--- typescript/index.js
+++ typescript/index.js
@@ -1,16 +1,18 @@
 var firstName = "Dylan"; // explicit typing (writing out type)
 var otherGuy = "Bobby"; // inplicit typing - will infer type based on assigned value
-// otherGuy = 9; // does not allow this
+// otherGuy = 9; // does not allow this due to static typing
 console.log(firstName);
 console.log(typeof firstName);
 function greet(val) {
     console.log("Hello, ".concat(val)); // note the backticks - called template literals (string interpolation)
     val = "Timmy"; // allowed
+    var someVal = val; // assignment using any is allowed
     console.log("Hello again but this time you are ".concat(val));
     // can be dangerous, since it ignores type errors
     // val.push("Bob");
 }
 greet(50);
+greet("Disney");
 // unknown is a safer alternative to any
 function greet1(val) {
     console.log("Hello, ".concat(val));
--- typescript/index.ts
+++ typescript/index.ts
@@ -1,22 +1,20 @@
 let firstName: string = "Dylan"; // explicit typing (writing out type)
 let otherGuy = "Bobby"; // inplicit typing - will infer type based on assigned value
-
-// otherGuy = 9; // does not allow this
-
+// otherGuy = 9; // does not allow this due to static typing
 console.log(firstName);
-
-console.log(typeof firstName)
+console.log(typeof firstName);
 
 function greet(val: any) {
     console.log(`Hello, ${val}`); // note the backticks - called template literals (string interpolation)
 
     val = "Timmy"; // allowed
+    let someVal: string = val; // assignment using any is allowed
     console.log(`Hello again but this time you are ${val}`);
-    
     // can be dangerous, since it ignores type errors
     // val.push("Bob");
 }
 greet(50);
+greet("Disney");
 
 // unknown is a safer alternative to any
 function greet1(val: unknown) {
